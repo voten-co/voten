58e82130c5f4941367b0027d15a1e6475ed14696
Sully Fischer <fischersully@gmail.com>
2017-05-26 15:21:59 +0430

    Runs Google Analytics code only in production mode

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -211,22 +211,24 @@ router.beforeEach((to, from, next) => {
  * is simple running after after navigating to the new routes.
  */
 router.afterEach((to, from) => {
-   (function(i, s, o, g, r, a, m) {
-       i['GoogleAnalyticsObject'] = r;
-       i[r] = i[r] || function() {
-           (i[r].q = i[r].q || []).push(arguments)
-       }, i[r].l = 1 * new Date();
-       a = s.createElement(o),
-           m = s.getElementsByTagName(o)[0];
-       a.async = 1;
-       a.src = g;
-       m.parentNode.insertBefore(a, m)
-   })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+	if (Laravel.env == 'production') {
+		(function(i, s, o, g, r, a, m) {
+	       i['GoogleAnalyticsObject'] = r;
+	       i[r] = i[r] || function() {
+	           (i[r].q = i[r].q || []).push(arguments)
+	       }, i[r].l = 1 * new Date();
+	       a = s.createElement(o),
+	           m = s.getElementsByTagName(o)[0];
+	       a.async = 1;
+	       a.src = g;
+	       m.parentNode.insertBefore(a, m)
+	   })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-   ga('create', 'UA-89431807-1', 'auto');
+	   ga('create', 'UA-89431807-1', 'auto');
 
-   ga('set', 'page', to.path);
-   ga('send', 'pageview');
+	   ga('set', 'page', to.path);
+	   ga('send', 'pageview');
+	}
 })
 
 
