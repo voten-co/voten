a808f3fa0527381459874a1aca57efee8d242129
Chris Breuer <chrisbreuer93@gmail.com>
2017-06-16 15:58:24 -0700

    Add Seeder for admin role and user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
     }
 }
