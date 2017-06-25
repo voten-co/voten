af1f063d3cebb3cab8e5891d8256b74e07203ddd
Sully Fischer <fischersully@gmail.com>
2017-06-25 23:11:51 +0430

    adds tooltip for sidebar toggle button

--- resources/assets/js/components/Menu-button.vue
+++ resources/assets/js/components/Menu-button.vue
@@ -1,23 +1,30 @@
 <template>
-    <button class="v-menu-button" :class="{'v-menu-button--checked' : checked}" @click="toggle">
+    <button class="v-menu-button" :class="{'v-menu-button--checked' : checked}" @click="toggle"
+    data-toggle="tooltip" data-placement="right" title="Toggle Sidebar">
         <span></span>
     </button>
 </template>
 
 <script>
-export default {
-    props: ['checked'],
+	export default {
+	    props: ['checked'],
 
-    methods: {
-    	/**
-    	 * Toggles the sidebar
-    	 *
-    	 * @return void
-    	 */
-    	toggle () {
-    		this.checked = ! this.checked
-    		this.$eventHub.$emit('toggle-sidebar')
-    	}
-    }
-}
+		mounted () {
+			this.$nextTick(function () {
+				this.$root.loadSemanticTooltip();
+			});
+		},
+
+	    methods: {
+	    	/**
+	    	 * Toggles the sidebar
+	    	 *
+	    	 * @return void
+	    	 */
+	    	toggle() {
+	    		this.checked = ! this.checked
+	    		this.$eventHub.$emit('toggle-sidebar')
+	    	}
+	    }
+	}
 </script>
