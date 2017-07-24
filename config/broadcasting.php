4924f934c225846ceb0e540611100ae5468caf4e
Sully Fischer <fischersully@gmail.com>
2017-07-24 07:59:18 +0000

    Apply fixes from StyleCI

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -47,8 +47,8 @@ return [
 
         'echo' => [
             'app_address'  => env('ECHO_ADDRESS'),
-            'app_id'  => env('ECHO_APP_ID'),
-            'auth_key'  => env('ECHO_AUTH_KEY'),
+            'app_id'       => env('ECHO_APP_ID'),
+            'auth_key'     => env('ECHO_AUTH_KEY'),
         ],
 
         'pusher' => [
