569e06146940620cc2efcd005dc01ffe61845315
Sully Fischer <fischersully@gmail.com>
2017-07-28 16:44:33 +0430

    basics of "hot conversations"

--- app/Traits/EchoServer.php
+++ app/Traits/EchoServer.php
@@ -29,4 +29,28 @@ trait EchoServer
             $this->echoAddress().'/apps/'.$this->echoAppId().'/status?auth_key='.$this->echoAuthKey()
         );
     }
+
+    public function echoHotConversations()
+    {
+        $list = externalJson(
+            $this->echoAddress().'/apps/'.$this->echoAppId().'/channels?auth_key='.$this->echoAuthKey()
+        );
+
+        $list = collect($list->channels);
+
+        // filter to submission public channels only
+        $list = $list->filter(function ($item, $key) {
+            return starts_with($key, 'submission.');
+        });
+
+        // map
+//        $list->transform(function ($item, $key) {
+//            return str_after($key, 'submission.');
+//        });
+
+        // sort
+        $list = $list->sortBy('subscription_count', 1, true);
+
+        return $list;
+    }
 }
