46effcff8b9f881a763467f93b8a0f44ee2d8366
Sully Fischer <fischersully@gmail.com>
2017-08-09 13:48:41 +0430

    installs WhichBrowser/Parser-PHP

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2017-07-28.
+    * Generated on 2017-08-09.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
