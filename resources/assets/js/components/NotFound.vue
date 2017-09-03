1219610f7567ec5b50e4474bec4439b7d2d1f182
Sully Fischer <fischersully@gmail.com>
2017-09-03 19:58:24 +0430

    Make the message funnier

--- resources/assets/js/components/NotFound.vue
+++ resources/assets/js/components/NotFound.vue
@@ -1,14 +1,20 @@
 <template>
 	<section class="container margin-top-1 col-7">
-		<no-content  :text="'Sorry, that page doesn’t exist!'"></no-content>
+		<no-content  :text="message"></no-content>
 	</section>
 </template>
 
 
 <script>
-	import NoContent from '../components/NoContent.vue'
+	import NoContent from '../components/NoContent.vue';
 
 	export default {
+	    data() {
+	        return {
+	            message: "I hate to be the one breaking it to you, but you've been given a wrong address!"
+			}
+		},
+
 	    components: {
 	    	NoContent
 	    }
