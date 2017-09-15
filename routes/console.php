3b19620a1b162ba8aa574561dd5fa07bb9879868
Sully Fischer <fischersully@gmail.com>
2017-09-16 00:53:12 +0430

    Implement "update-submissions" command

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+Artisan::command('update-submissions', function () {
+    $submissions = \App\Submission::all();
+
+    foreach ($submissions as $submission) {
+        $submission->update([
+            'url'    => $submission->type === 'link' ? $submission->data['url'] : config('app.url') . '/c/' . $submission->category_name . '/' . $submission->slug,
+            'domain' => $submission->type === 'link' ? domain($submission->data['url']) : null,
+        ]);
+    }
+
+    $this->info($submissions->count() . ' records have been successfully updated.');
+})->describe('Fill "url" and "domain" fields.');
