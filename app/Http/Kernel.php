c3505e0d1de0ff00697ad7d5c14906b9b744b44e
G_will <g_will@ieqi.com>
2017-09-18 11:47:33 +0800

    fixes for laravel 5.5

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,8 +17,11 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\TrustProxies::class,
         Firewall::class,
     ];
 
