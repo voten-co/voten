938ec5a4e06b851b063271fb806a48fb0d44d192
Sully Fischer <fischersully@gmail.com>
2017-09-27 17:09:46 +0330

    Tooltip for sidebar items

--- resources/assets/js/components/Sidebar.vue
+++ resources/assets/js/components/Sidebar.vue
@@ -1,47 +1,40 @@
 <template>
     <div class="side-fixed" id="v-sidebar">
 		<div class="sidebar-left">
-			<div class="item">
-				<router-link :to="'/' + '@' + auth.username" :class="{ 'active': ($route.name == 'user-submissions' || $route.name == 'user-comments' || $route.name == 'user-upvotes' || $route.name == 'user-downvotes') }">
-					<img :src="auth.avatar" :alt="auth.username" class="avatar">
-				</router-link>
-			</div>
-			<div class="item">
-				<router-link :to="'/'" :class="{ 'active': $route.name == 'home' }">
-					<home-icon width="30" height="30"></home-icon>
-				</router-link>
-			</div>
-			<div class="item">
-				<router-link :to="{ path: '/bookmarks' }" active-class="active">
-					<bookmark-icon width="30" height="30"></bookmark-icon>
-				</router-link>
-			</div>
-
-			<div class="item">
-				<router-link :to="{ path: submitURL }" active-class="active">
-					<submit-icon width="30" height="30"></submit-icon>
-				</router-link>
-			</div>
-			<div class="item">
-				<router-link :to="{ path: '/@' + auth.username + '/settings' }" active-class="active">
-					<settings-icon width="30" height="30"></settings-icon>
-				</router-link>
-			</div>
-			<div class="item">
-				<router-link :to="{ path: '/find-channels' }" active-class="active">
-					<search-icon width="30" height="30"></search-icon>
-				</router-link>
-			</div>
-			<div class="item">
-				<router-link :to="{ path: '/channel' }" active-class="active">
-					<channel-icon width="30" height="30"></channel-icon>
-				</router-link>
-			</div>
-			<div class="item">
-				<router-link :to="{ path: '/live' }" active-class="active">
-					<chat-icon width="30" height="30"></chat-icon>
-				</router-link>
-			</div>
+			<router-link :to="'/' + '@' + auth.username"
+						 :class="{ 'active': ($route.name == 'user-submissions' || $route.name == 'user-comments' || $route.name == 'user-upvotes' || $route.name == 'user-downvotes') }"
+						 class="item"
+						v-tooltip.right="{content: 'Profile', offset: 0}">
+				<img :src="auth.avatar" :alt="auth.username" class="avatar">
+			</router-link>
+			<router-link :to="'/'" :class="{ 'active': $route.name == 'home' }" class="item"
+						v-tooltip.right="{content: 'Home Feed', offset: 0}">
+				<home-icon width="30" height="30"></home-icon>
+			</router-link>
+			<router-link :to="{ path: '/bookmarks' }" active-class="active" class="item"
+						v-tooltip.right="{content: 'Bookmarks', offset: 0}">
+				<bookmark-icon width="30" height="30"></bookmark-icon>
+			</router-link>
+			<router-link :to="{ path: submitURL }" active-class="active" class="item"
+						v-tooltip.right="{content: 'Submit Content', offset: 0}">
+				<submit-icon width="30" height="30"></submit-icon>
+			</router-link>
+			<router-link :to="{ path: '/@' + auth.username + '/settings' }" active-class="active" class="item"
+						v-tooltip.right="{content: 'Settings', offset: 0}">
+				<settings-icon width="30" height="30"></settings-icon>
+			</router-link>
+			<router-link :to="{ path: '/find-channels' }" active-class="active" class="item"
+						v-tooltip.right="{content: 'Find Channels', offset: 0}">
+				<search-icon width="30" height="30"></search-icon>
+			</router-link>
+			<router-link :to="{ path: '/channel' }" active-class="active" class="item"
+						v-tooltip.right="{content: 'Create New Channel', offset: 0}">
+				<channel-icon width="30" height="30"></channel-icon>
+			</router-link>
+			<router-link :to="{ path: '/live' }" active-class="active" class="item"
+						v-tooltip.right="{content: 'Live (coming soon)', offset: 0}">
+				<chat-icon width="30" height="30"></chat-icon>
+			</router-link>
 		</div>
 
 		<div class="sidebar-right">
