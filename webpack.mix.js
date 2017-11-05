626cfec211958ede4b6048d546f7335b0998e989
Sully Fischer <fischersully@gmail.com>
2017-11-05 21:54:32 +0330

    Get "npm run hot" working again

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,33 +1,35 @@
 const { mix } = require('laravel-mix');
 
-
 mix.js('resources/assets/js/app.js', 'public/js')
-	.js('resources/assets/js/landing.js', 'public/js')
-	.js('resources/assets/js/backend.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/admin.scss', 'public/css')
-    .sourceMaps()
-    .extract([
-        'vue',
-        'axios',
-        'lodash',
-        'jquery',
-        'vue-ua',
-        'autosize',
-        'video.js',
-        'pusher-js',
-        'vue-focus',
-        'vue-router',
-        'laravel-echo',
-        'vue-clickaway',
-        'vue-multiselect',
-        'moment-timezone',
-        'vue-template-compiler',
-    ])
-    .autoload({
-        vue : 'Vue',
-        lodash : '_',
-        'pusher-js' : 'Pusher',
-        jquery: ['$', 'jQuery'],
-    })
-    .version()
+   .js('resources/assets/js/landing.js', 'public/js')
+   .js('resources/assets/js/backend.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .sass('resources/assets/sass/admin.scss', 'public/css')
+   .sourceMaps()
+   .extract([
+       'vue',
+       'axios',
+       'lodash',
+       'jquery',
+       'vue-ua',
+       'autosize',
+       'video.js',
+       'pusher-js',
+       'vue-router',
+       'laravel-echo',
+       'element-ui',
+       'vue-clickaway',
+       'moment-timezone',
+       'vue-template-compiler',
+   ])
+   .autoload({
+       vue: 'Vue',
+       lodash: '_',
+       'pusher-js': 'Pusher',
+       jquery: ['$', 'jQuery'],
+   });
+
+// run versioning on production only
+if (mix.inProduction()) {
+    mix.version();
+}
