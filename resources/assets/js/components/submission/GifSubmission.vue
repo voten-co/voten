ecd2f8626b4ffb5682d7aacb630b45cab28f0f84
Sully Fischer <fischersully@gmail.com>
2018-01-13 18:45:18 +0330

    improve UX

--- resources/assets/js/components/submission/GifSubmission.vue
+++ resources/assets/js/components/submission/GifSubmission.vue
@@ -1,87 +1,125 @@
+<style lang="scss">
+.gif-submission {
+    max-height: 500px;
+
+    img {
+        max-height: 500px;
+    }
+}
+</style>
+
+
 <template>
-    <div>
-        <div v-if="showBigThumbnail" class="align-center relative pointer" @click="$emit('play-gif')">
-            <img :src="submission.data.thumbnail_path" :alt="submission.title" class="big-thumbnail"/>
+	<div>
+		<div v-if="showBigThumbnail"
+		     class="align-center gif-submission">
+			<video loop
+			       controls
+			       autoplay
+			       :poster="submission.data.thumbnail_path"
+                   onclick="this.paused ? this.play() : this.pause();">
+				<source :src="submission.data.mp4_path"
+				        type="video/mp4">
+			</video>
+		</div>
 
-            <span class="play-gif">
-                <i class="v-icon v-gif-1"></i>
-            </span>
-        </div>
-
-        <div class="link-list-info">
-            <span class="submission-img-title">
-	            <a class="submisison-small-thumbnail" v-if="submission.data.thumbnail_path && !full">
-	            	<!-- img -->
+		<div class="link-list-info">
+			<span class="submission-img-title">
+				<a class="submisison-small-thumbnail"
+				   v-if="submission.data.thumbnail_path && !full">
+					<!-- img -->
 					<div :style="thumbnail"
-                         v-if="showSmallThumbnail" class="small-thumbnail zoom-in" @click="$emit('play-gif')"></div>
-	            </a>
+					     v-if="showSmallThumbnail"
+					     class="small-thumbnail zoom-in"
+					     @click="$emit('play-gif')"></div>
+				</a>
 
-				<h1 class="submission-title" v-if="full">
-					<router-link :to="'/c/' + submission.channel_name + '/' + submission.slug"
-                                 class="flex-space v-ultra-bold">
-	                    {{ submission.title }}
-	                </router-link>
+				<h1 class="submission-title"
+				    v-if="full">
+                    <el-tooltip content="NSFW" placement="bottom" transition="false" :open-delay="500">
+                        <i class="v-icon v-shocked go-red" aria-hidden="true" v-if="submission.nsfw"></i>
+                    </el-tooltip>
+
+					{{ submission.title }}
 				</h1>
 
-                <div class="flex1" v-else>
+				<div class="flex1"
+				     v-else>
 					<router-link :to="'/c/' + submission.channel_name + '/' + submission.slug"
-                                 class="flex-space v-ultra-bold">
-	                    {{ submission.title }}
-	                </router-link>
+					             class="flex-space v-ultra-bold">
+						{{ submission.title }}
+					</router-link>
 
-					<submission-footer :url="url" :comments="comments" :bookmarked="bookmarked" :submission="submission"
-                                       v-if="!full"
-                                       @bookmark="$emit('bookmark')" @report="$emit('report')" @hide="$emit('hide')"
-                                       @nsfw="$emit('nsfw')" @sfw="$emit('sfw')" @destroy="$emit('destroy')"
-                                       @approve="$emit('approve')" @disapprove="$emit('disapprove')"
-                                       @removethumbnail="$emit('removethumbnail')" :upvoted="upvoted"
-                                       :downvoted="downvoted" :points="points"
-                                       @upvote="$emit('upvote')" @downvote="$emit('downvote')"
-                    ></submission-footer>
+					<submission-footer :url="url"
+					                   :comments="comments"
+					                   :bookmarked="bookmarked"
+					                   :submission="submission"
+					                   v-if="!full"
+					                   @bookmark="$emit('bookmark')"
+					                   @report="$emit('report')"
+					                   @hide="$emit('hide')"
+					                   @nsfw="$emit('nsfw')"
+					                   @sfw="$emit('sfw')"
+					                   @destroy="$emit('destroy')"
+					                   @approve="$emit('approve')"
+					                   @disapprove="$emit('disapprove')"
+					                   @removethumbnail="$emit('removethumbnail')"
+					                   :upvoted="upvoted"
+					                   :downvoted="downvoted"
+					                   :points="points"
+					                   @upvote="$emit('upvote')"
+					                   @downvote="$emit('downvote')"></submission-footer>
 				</div>
-            </span>
-        </div>
-    </div>
+			</span>
+		</div>
+	</div>
 </template>
 
 <script>
-    import SubmissionFooter from '../../components/SubmissionFooter.vue';
+import SubmissionFooter from '../../components/SubmissionFooter.vue';
 
-    export default {
-        components: { SubmissionFooter },
+export default {
+    components: { SubmissionFooter },
 
-        data() {
+    data() {
+        return {
+            auth
+        };
+    },
+
+    props: {
+        nsfw: {},
+        submission: {},
+        bookmarked: {},
+        url: {},
+        comments: {},
+        upvoted: {},
+        downvoted: {},
+        points: {},
+        full: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    computed: {
+        thumbnail() {
             return {
-                auth
-            }
+                backgroundImage: 'url(' + this.submission.data.thumbnail_path + ')'
+            };
         },
 
-        props: {
-            nsfw: {}, submission: {}, bookmarked: {}, url: {}, comments: {}, upvoted: {}, downvoted: {}, points: {},
-            full: {
-                type: Boolean,
-                default: false,
-            },
+        showBigThumbnail() {
+            if (this.full) return true;
+
+            if (this.nsfw) return false;
+
+            return !auth.submission_small_thumbnail;
         },
 
-        computed: {
-            thumbnail() {
-                return {
-                    backgroundImage: 'url(' + this.submission.data.thumbnail_path + ')'
-                };
-            },
-
-            showBigThumbnail(){
-                if (this.full) return true
-
-                if (this.nsfw) return false
-
-                return !auth.submission_small_thumbnail
-            },
-
-            showSmallThumbnail(){
-                return !this.showBigThumbnail && !this.nsfw
-            }
-        },
-    };
+        showSmallThumbnail() {
+            return !this.showBigThumbnail && !this.nsfw;
+        }
+    }
+};
 </script>
--- resources/assets/js/components/submission/ImgSubmission.vue
+++ resources/assets/js/components/submission/ImgSubmission.vue
@@ -1,33 +1,92 @@
+<style lang="scss">
+.img .profile-post-content img {
+    cursor: zoom-in;
+    height: 100%;
+    width: auto;
+}
+</style>
+
+
 <template>
 	<div>
 		<div v-if="!isAlbum && showBigThumbnail">
-			<img :src="submission.data.thumbnail_path" :alt="submission.title" @click="$emit('zoom')" class="big-thumbnail"/>
+			<img :src="submission.data.thumbnail_path"
+			     :alt="submission.title"
+			     @click="$emit('zoom', submission.data)"
+			     class="big-thumbnail" />
 		</div>
 
 		<div v-if="isAlbum && showBigThumbnail">
-			<img :src="value.thumbnail_path" v-for="(value, index) in photos" :key="value.id"
-			@click="$emit('zoom', index)" :alt="submission.title" class="margin-bottom-1" />
+			<el-carousel :height="isMobile ? '200px' : '500px'"
+			             :autoplay="false"
+			             arrow="always">
+				<el-carousel-item v-for="item in photos"
+				                  :key="item.id">
+					<img :src="item.thumbnail_path"
+					     @click="$emit('zoom', item)"
+					     :alt="submission.title">
+				</el-carousel-item>
+			</el-carousel>
+
+			<!-- <img :src="value.thumbnail_path"
+			     v-for="(value, index) in photos"
+			     :key="value.id"
+			     @click="$emit('zoom', index)"
+			     :alt="submission.title"
+			     class="margin-bottom-1" /> -->
 		</div>
 
 		<div class="link-list-info">
 			<span class="submission-img-title">
-				<a class="submisison-small-thumbnail" v-if="submission.data.thumbnail_path && !full">
+				<a class="submisison-small-thumbnail"
+				   v-if="submission.data.thumbnail_path && !full">
 					<!-- img -->
-					<div v-if="showSmallThumbnail" class="small-thumbnail zoom-in" :style="thumbnail"
-					@click="$emit('zoom')"></div>
+					<div v-if="showSmallThumbnail"
+					     class="small-thumbnail zoom-in"
+					     :style="thumbnail"
+					     @click="$emit('zoom', submission.data)"></div>
 				</a>
 
 				<div class="flex1">
-					<router-link :to="'/c/' + submission.channel_name + '/' + submission.slug" 
-						class="flex-space v-ultra-bold" :class="{'submission-title': full}"
-					>
+					<router-link :to="'/c/' + submission.channel_name + '/' + submission.slug"
+					             class="flex-space v-ultra-bold"
+					             v-if="!full">
 						{{ submission.title }}
 					</router-link>
 
-					<submission-footer :url="url" :comments="comments" :bookmarked="bookmarked" :submission="submission" v-if="!full"
-						@bookmark="$emit('bookmark')" @report="$emit('report')" @hide="$emit('hide')" @nsfw="$emit('nsfw')" @sfw="$emit('sfw')" @destroy="$emit('destroy')" @approve="$emit('approve')" @disapprove="$emit('disapprove')" @removethumbnail="$emit('removethumbnail')" :upvoted="upvoted" :downvoted="downvoted" :points="points"
-						@upvote="$emit('upvote')" @downvote="$emit('downvote')"
-					></submission-footer>
+					<h3 class="submission-title"
+					    v-else>
+						<el-tooltip content="NSFW"
+						            placement="bottom"
+						            transition="false"
+						            :open-delay="500">
+							<i class="v-icon v-shocked go-red"
+							   aria-hidden="true"
+							   v-if="submission.nsfw"></i>
+						</el-tooltip>
+
+						{{ submission.title }}
+					</h3>
+
+					<submission-footer :url="url"
+					                   :comments="comments"
+					                   :bookmarked="bookmarked"
+					                   :submission="submission"
+					                   v-if="!full"
+					                   @bookmark="$emit('bookmark')"
+					                   @report="$emit('report')"
+					                   @hide="$emit('hide')"
+					                   @nsfw="$emit('nsfw')"
+					                   @sfw="$emit('sfw')"
+					                   @destroy="$emit('destroy')"
+					                   @approve="$emit('approve')"
+					                   @disapprove="$emit('disapprove')"
+					                   @removethumbnail="$emit('removethumbnail')"
+					                   :upvoted="upvoted"
+					                   :downvoted="downvoted"
+					                   :points="points"
+					                   @upvote="$emit('upvote')"
+					                   @downvote="$emit('downvote')"></submission-footer>
 				</div>
 			</span>
 		</div>
@@ -35,68 +94,84 @@
 </template>
 
 <script>
-	import SubmissionFooter from '../../components/SubmissionFooter.vue';
+import SubmissionFooter from '../../components/SubmissionFooter.vue';
+import Helpers from '../../mixins/Helpers';
 
-	export default {
-		components: {
-			SubmissionFooter
-		},
+export default {
+    components: {
+        SubmissionFooter, 
+    },
 
-	    props: {
-	    	nsfw: {}, submission: {}, bookmarked:{}, url: {}, comments: {}, upvoted: {}, downvoted: {}, points: {},
-	        full: {
-	            type: Boolean,
-	            default: false,
-	        },
-	    },
+    mixins: [Helpers],
 
-	    data: function () {
-	        return {
-				auth,
-	            photos: [],
-	        };
-	    },
+    props: {
+        nsfw: {},
+        submission: {},
+        bookmarked: {},
+        url: {},
+        comments: {},
+        upvoted: {},
+        downvoted: {},
+        points: {},
+        full: {
+            type: Boolean,
+            default: false
+        }
+    },
 
-		computed: {
-			thumbnail() {
-				return {
-					backgroundImage: 'url(' + this.submission.data.thumbnail_path + ')'
-				};
-			},
+    data() {
+        return {
+            photos: [],
+            currentImageIndex: 0
+        };
+    },
 
-			isAlbum(){
-				return this.photos.length > 1
-			},
+    computed: {
+        thumbnail() {
+            return {
+                backgroundImage: 'url(' + this.submission.data.thumbnail_path + ')'
+            };
+        },
 
-			showBigThumbnail(){
-				if (this.full) return true
+        isAlbum() {
+            return this.photos.length > 1;
+        },
 
-				if (this.nsfw) return false
+        showBigThumbnail() {
+            if (this.full) return true;
 
-				return ! auth.submission_small_thumbnail
-			},
+            if (this.nsfw) return false;
 
-			showSmallThumbnail(){
-				return ! this.showBigThumbnail && !this.nsfw
-			}
-		},
+            return !auth.submission_small_thumbnail;
+        },
 
-	    created: function() {
-	        if(this.full){
-	        	this.getPhotos()
-	        }
-	    },
+        showSmallThumbnail() {
+            return !this.showBigThumbnail && !this.nsfw;
+        }
+    },
 
-	    methods: {
-	    	getPhotos: function () {
-	    		axios.get('/submission-photos', {
-	    			params: {
-	    				id: this.submission.id
-	    			}
-	    		}).then((response) => {
-	                this.photos = response.data
-	            });
-	    	},
-	    }
-	}
+    created: function() {
+        if (this.full) {
+            this.getPhotos();
+        }
+    },
+
+    methods: {
+        setCurrentImage(index) {
+            this.currentImageIndex = index;
+        },
+
+        getPhotos: function() {
+            axios
+                .get('/submission-photos', {
+                    params: {
+                        id: this.submission.id
+                    }
+                })
+                .then(response => {
+                    this.photos = response.data;
+                });
+        }
+    }
+};
 </script>
