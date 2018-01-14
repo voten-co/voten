32fdf8484265a90bdb6724a0c0dd904bd270befb
Sully Fischer <fischersully@gmail.com>
2018-01-14 22:07:39 +0330

    fix a bug

--- resources/assets/js/components/NoContent.vue
+++ resources/assets/js/components/NoContent.vue
@@ -1,56 +1,60 @@
 <template>
 	<transition name="el-fade-in-linear">
-		<div class="no-content-wrapper user-select" v-show="visible">
-			<component :is="currentView" width="160" height="160"></component>
-			
-	        <p v-text="text"></p>
+		<div class="no-content-wrapper user-select"
+		     v-show="visible">
+			<component :is="currentView"
+			           width="160"
+			           height="160"></component>
+
+			<p v-text="text"></p>
 		</div>
 	</transition>
 </template>
 
 
 <script>
-import SubmissionIcon from "./Icons/SubmissionIcon.vue";
-import CommentIcon from "./Icons/ChatIcon.vue";
-import ChannelIcon from "./Icons/ChannelIcon.vue";
-import UserIcon from "./Icons/UserIcon.vue";
+import SubmissionIcon from './Icons/SubmissionIcon.vue';
+import CommentIcon from './Icons/ChatIcon.vue';
+import ChannelIcon from './Icons/ChannelIcon.vue';
+import UserIcon from './Icons/UserIcon.vue';
 
 export default {
-  props: ['text', 'icon'],
+    props: ['text', 'icon'],
 
-  components: {
-    SubmissionIcon,
-	ChannelIcon, 
-	UserIcon
-  },
+    components: {
+        SubmissionIcon,
+        CommentIcon,
+        ChannelIcon,
+        UserIcon
+    },
 
-  data() {
-    return {
-      visible: true
-    };
-  },
+    data() {
+        return {
+            visible: true
+        };
+    },
 
-  computed: {
-    currentView() {
-      switch (this.icon) {
-        case "comment":
-          return "CommentIcon";
-          break;
+    computed: {
+        currentView() {
+            switch (this.icon) {
+                case 'comment':
+                    return 'CommentIcon';
+                    break;
 
-        case "channel":
-          return "ChannelIcon";
-          break;
+                case 'channel':
+                    return 'ChannelIcon';
+                    break;
 
-        case "user":
-          return "UserIcon";
-          break;
+                case 'user':
+                    return 'UserIcon';
+                    break;
 
-        default:
-          // 'submission'
-          return "SubmissionIcon";
-          break;
-      }
+                default:
+                    // 'submission'
+                    return 'SubmissionIcon';
+                    break;
+            }
+        }
     }
-  }
 };
 </script>
