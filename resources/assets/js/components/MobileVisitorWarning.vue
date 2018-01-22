577188733ecd8cf0564aafa2ac1b207cd34d0814
Sully Fischer <fischersully@gmail.com>
2018-01-22 21:26:47 +0330

    npm run production

---
+++ resources/assets/js/components/MobileVisitorWarning.vue
@@ -0,0 +1,39 @@
+<template>
+<p>
+    Since beta 2.0, Voten's web client is no longer mobile-friendly. Instead, we are focusing on developing native mobile apps to bring a truly outstanding experience for our mobile using users. 
+</p>
+</template>
+
+<script>
+    export default {
+        components: {},
+
+        mixins: [],
+
+        data () {
+            return {
+                //
+            }
+        },
+
+        props: {
+            //
+        },
+
+        computed: {
+            //
+        },
+
+        created () {
+            //
+        },
+
+        mounted () {
+            //
+        },
+
+        methods: {
+            //
+        }
+    };
+</script>
