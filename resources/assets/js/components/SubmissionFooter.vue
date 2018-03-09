fcaa35527f45802e7c64ab29f6c2a3d00a6b8645
Sully Fischer <fischersully@gmail.com>
2018-03-09 16:53:22 +0330

    resolve #317

--- resources/assets/js/components/SubmissionFooter.vue
+++ resources/assets/js/components/SubmissionFooter.vue
@@ -2,82 +2,122 @@
 	<div class="index-submission-footer user-select">
 		<div class="display-inline">
 			<div class="display-inline">
-				<el-tooltip class="item" content="Comments" placement="top" transition="false" :open-delay="500">
-					<router-link :to="url" class="comments-icon h-green">
-						<i class="v-icon v-comment"></i><span v-if="comments" v-text="comments"></span>
+				<el-tooltip class="item"
+				            content="Comments"
+				            placement="top"
+				            transition="false"
+				            :open-delay="500">
+					<router-link :to="url"
+					             class="comments-icon h-green">
+						<i class="v-icon v-comment"></i>
+						<span v-if="comments"
+						      v-text="comments"></span>
 					</router-link>
 				</el-tooltip>
-				
-				<el-tooltip class="item" :content="bookmarked ? 'Unbookmark' : 'Bookmark'" placement="top" transition="false" :open-delay="500">
+
+				<el-tooltip class="item"
+				            :content="bookmarked ? 'Unbookmark' : 'Bookmark'"
+				            placement="top"
+				            transition="false"
+				            :open-delay="500">
 					<a @click="$emit('bookmark')">
-						<i class="v-icon h-yellow pointer" :class="bookmarked ? 'go-yellow v-unbookmark' : 'v-bookmark'"></i>
+						<i class="v-icon h-yellow pointer"
+						   :class="bookmarked ? 'go-yellow v-unbookmark' : 'v-bookmark'"></i>
 					</a>
 				</el-tooltip>
-				
-				<el-dropdown size="mini" type="primary" trigger="click" :show-timeout="0" :hide-timeout="0">
+
+				<el-dropdown size="mini"
+				             type="primary"
+				             trigger="click"
+				             :show-timeout="0"
+				             :hide-timeout="0">
 					<i class="el-icon-more-outline"></i>
 
 					<el-dropdown-menu slot="dropdown">
-						<el-dropdown-item v-if="!owns" @click.native="$emit('report')">
+						<el-dropdown-item v-if="!owns"
+						                  @click.native="$emit('report')">
 							Report
 						</el-dropdown-item>
-						
-						<el-dropdown-item @click.native="$emit('hide')" v-if="!owns">
+
+						<el-dropdown-item @click.native="$emit('hide')"
+						                  v-if="!owns">
 							Hide
 						</el-dropdown-item>
 
-						<el-dropdown-item @click.native="$emit('nsfw')" v-if="showNSFW">
+						<el-dropdown-item @click.native="$emit('nsfw')"
+						                  v-if="showNSFW">
 							NSFW
 						</el-dropdown-item>
-						
-						<el-dropdown-item @click.native="$emit('sfw')" v-if="showSFW">
+
+						<el-dropdown-item @click.native="$emit('sfw')"
+						                  v-if="showSFW">
 							Family Safe
 						</el-dropdown-item>
 
-						<el-dropdown-item class="go-red" @click.native="$emit('destroy')" v-if="owns">
+						<el-dropdown-item class="go-red"
+						                  @click.native="$emit('destroy')"
+						                  v-if="owns">
 							Delete
 						</el-dropdown-item>
-						
-						<el-dropdown-item class="go-green" @click.native="$emit('approve')" v-if="showApprove" divided>
+
+						<el-dropdown-item class="go-green"
+						                  @click.native="$emit('approve')"
+						                  v-if="showApprove"
+						                  divided>
 							Approve
 						</el-dropdown-item>
-						
-						<el-dropdown-item class="go-red" @click.native="$emit('disapprove')" v-if="showDisapprove">
+
+						<el-dropdown-item class="go-red"
+						                  @click.native="$emit('disapprove')"
+						                  v-if="showDisapprove">
 							Delete
 						</el-dropdown-item>
 					</el-dropdown-menu>
 				</el-dropdown>
 			</div>
 
-			<div class="voting-wrapper display-none mobile-only" v-if="isMobile">
-				<a class="fa-stack align-right" @click="$emit('upvote')">
-					<i class="v-icon v-up-fat" :class="upvoted ? 'go-primary animated bounceIn' : 'go-gray'"></i>
+			<div class="voting-wrapper display-none mobile-only"
+			     v-if="isMobile">
+				<a class="fa-stack align-right"
+				   @click="$emit('upvote')">
+					<i class="v-icon v-up-fat"
+					   :class="upvoted ? 'go-primary animated bounceIn' : 'go-gray'"></i>
 				</a>
 
 				<div class="detail">
 					{{ points }} Points
 				</div>
 
-				<a class="fa-stack align-right" @click="$emit('downvote')">
-					<i class="v-icon v-down-fat" :class="downvoted ? 'go-red animated bounceIn' : 'go-gray'"></i>
+				<a class="fa-stack align-right"
+				   @click="$emit('downvote')">
+					<i class="v-icon v-down-fat"
+					   :class="downvoted ? 'go-red animated bounceIn' : 'go-gray'"></i>
 				</a>
 			</div>
 		</div>
 
-		<span class="desktop-only" v-if="! isMobile">
+		<span class="desktop-only"
+		      v-if="! isMobile">
 			Submitted {{ date }} by
-			<router-link :to="'/' + '@' + submission.author.username" class="h-underline desktop-only">
+			<router-link :to="'/' + '@' + submission.author.username"
+			             class="h-underline desktop-only">
 				{{ '@' + submission.author.username }}
 			</router-link>
-			to <router-link :to="'/c/' + submission.channel_name" class="channel-label h-underline">#{{ submission.channel_name }}</router-link>
+			to
+			<router-link :to="'/c/' + submission.channel_name"
+			             class="channel-label h-underline">#{{ submission.channel_name }}</router-link>
 		</span>
 
-		<div class="mobile-only mobile-submission-item-action" v-if="isMobile">
+		<div class="mobile-only mobile-submission-item-action"
+		     v-if="isMobile">
 			{{ date }} by
-			<router-link :to="'/' + '@' + submission.author.username" class="h-underline">
+			<router-link :to="'/' + '@' + submission.author.username"
+			             class="h-underline">
 				{{ '@' + submission.author.username }}
 			</router-link>
-			to <router-link :to="'/c/' + submission.channel_name" class="channel-label h-underline">#{{ submission.channel_name }}</router-link>
+			to
+			<router-link :to="'/c/' + submission.channel_name"
+			             class="channel-label h-underline">#{{ submission.channel_name }}</router-link>
 		</div>
 	</div>
 </template>
@@ -88,15 +128,7 @@ import Helpers from '../mixins/Helpers';
 export default {
     mixins: [Helpers],
 
-    props: [
-        'url',
-        'comments',
-        'bookmarked',
-        'submission',
-        'upvoted',
-        'downvoted',
-        'points'
-    ],
+    props: ['url', 'comments', 'bookmarked', 'submission', 'upvoted', 'downvoted', 'points'],
 
     computed: {
         /**
@@ -109,50 +141,57 @@ export default {
         },
 
         showApprove() {
-            return (
-                !this.submission.approved_at &&
-                (Store.state.moderatingAt.indexOf(this.submission.channel_id) !=
-                    -1 ||
-                    meta.isVotenAdminstrator) &&
-                !this.owns
-            );
+            try {
+                return (
+                    !this.submission.approved_at &&
+                    (Store.state.moderatingAt.indexOf(this.submission.channel_id) != -1 || meta.isVotenAdminstrator) &&
+                    !this.owns
+                );
+            } catch (error) {
+                return false; 
+            }
         },
 
         showDisapprove() {
-            return (
-                !this.submission.deleted_at &&
-                (Store.state.moderatingAt.indexOf(this.submission.channel_id) !=
-                    -1 ||
-                    meta.isVotenAdminstrator) &&
-                !this.owns
-            );
+            try {
+                return (
+                    !this.submission.deleted_at &&
+                    (Store.state.moderatingAt.indexOf(this.submission.channel_id) != -1 || meta.isVotenAdminstrator) &&
+                    !this.owns
+                );
+            } catch (error) {
+                return false;                 
+            }
         },
 
         showNSFW() {
-            return (
-                (this.owns ||
-                    Store.state.moderatingAt.indexOf(
-                        this.submission.channel_id
-                    ) != -1 ||
-                    meta.isVotenAdminstrator) &&
-                !this.submission.nsfw
-            );
+            try {
+                return (
+                    (this.owns ||
+                        Store.state.moderatingAt.indexOf(this.submission.channel_id) != -1 ||
+                        meta.isVotenAdminstrator) &&
+                    !this.submission.nsfw
+                );
+            } catch (error) {
+                return false; 
+            }
         },
 
         showSFW() {
-            return (
-                (this.owns ||
-                    Store.state.moderatingAt.indexOf(
-                        this.submission.channel_id
-                    ) != -1 ||
-                    meta.isVotenAdminstrator) &&
-                this.submission.nsfw
-            );
+            try {
+                return (
+                    (this.owns ||
+                        Store.state.moderatingAt.indexOf(this.submission.channel_id) != -1 ||
+                        meta.isVotenAdminstrator) &&
+                    this.submission.nsfw
+                );
+            } catch (error) {
+                return false; 
+            }
         },
 
         showRemoveTumbnail() {
-            if (this.owns && this.submission.content.thumbnail) return true;
-            return false;
+            return this.owns && this.submission.content.thumbnail ? true : false; 
         },
 
         date() {
